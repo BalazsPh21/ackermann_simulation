e3159afdb1565bd0445fd77f20080c9090c9834c
BalazsPh21 <balazsph@outlook.hu>
2023-12-16 11:31:32 +0100

    Issue with world

--- arv_bringup/launch/arv_bringup.launch.py
+++ arv_bringup/launch/arv_bringup.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
     # Load the SDF file from "description" package
-    sdf_file  =  os.path.join(pkg_project_description, 'models', 'model.sdf')
+    sdf_file  =  os.path.join(pkg_project_description, 'models', 'arv', 'model.sdf')
     with open(sdf_file, 'r') as infp:
         robot_desc = infp.read()
 
@@ -47,8 +47,18 @@ def generate_launch_description():
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_gazebo,
             'worlds',
-            'diff_drive.sdf'
+            'arv.sdf'
         ])}.items(),
+    )
+
+    # For publishing and controlling the robot pose, we need joint states of the robot
+    # Configure the robot model by adjusting the joint angles using the GUI slider
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        arguments=[sdf_file],
+        output=['screen']
     )
 
     # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
@@ -87,6 +97,7 @@ def generate_launch_description():
         DeclareLaunchArgument('rviz', default_value='true',
                               description='Open RViz.'),
         bridge,
+        joint_state_publisher_gui,
         arv_robot_state_publisher,
         rviz
     ])
